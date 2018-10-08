22b64d5c3e30ecdd32c9033f215895d7a0a16744
licaibin <951525835@qq.com>
2018-10-08 21:31:24 +0800

    homework4

---
+++ Homework4/Program1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+
+    public delegate void ClockEventHandler(object sender, AlarmclockEventArgs e);    //声明委托
+
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            string time = "";            
+            Console.WriteLine("请输入一个符合格式的时间，格式为 10/01/2018 10:01 （月/日/年 时间）：");
+            time = Console.ReadLine();
+            Alarm alarm = new Alarm();
+            alarm.Alarming += ShowAlarm;          
+            alarm.AlarmTime = DateTime.Parse(time);          
+            alarm.NewAlarm();         
+        }
+
+        static void ShowAlarm(object sender, AlarmclockEventArgs e)
+        {
+            Console.WriteLine("现在是北京时间：" + e.t.ToString());
+        }
+
+    }
+
+    public class AlarmclockEventArgs : EventArgs
+         {
+                  public DateTime t;
+          }   
+
+    public class Alarm
+    {
+        public event ClockEventHandler Alarming;
+        private DateTime time = DateTime.Now;      
+        public DateTime AlarmTime = DateTime.Today;
+        public void NewAlarm()
+        {        
+                while (time < AlarmTime)
+                         time = DateTime.Now;            
+                AlarmclockEventArgs alarmclockEventArgs = new AlarmclockEventArgs();
+                alarmclockEventArgs.t = time;
+                Alarming(this, alarmclockEventArgs);                     
+        }
+    }
+}
---
+++ Homework4/Program2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              //订单 
+              //未完成  下次补交
+        }
+    }
+}
